37d8cb5c704b7deec39fe1d5511bc97eaa2b00d1
ladykate17 <stamm.katherine@gmail.com>
2015-07-20 16:16:31 -0600

    Bonus I complete

--- colorutil.js
+++ colorutil.js
@@ -8,6 +8,16 @@ module.exports = {
 		var result = 0.2126*r + 0.7152*g + 0.0722*b;
 		return result;
 		
+	},
+
+	darken : function(r, g, b){
+
+		r = (r * 0.8);
+		g = (g * 0.8);
+		b = (b * 0.8);
+
+		var result = r + ' ' + g + ' ' + b;
+		return result;
 	}
 
 
