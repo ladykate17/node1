9c410e3565b773a2504155d4d9168ab5123317c4
ladykate17 <stamm.katherine@gmail.com>
2015-07-21 14:40:29 -0600

    Bonus II complete

--- BonusII/webcolor.js
+++ BonusII/webcolor.js
@@ -5,24 +5,26 @@ var _ = require('underscore');
 
 request('https://cdn.rawgit.com/metaraine/swatch/74580660c9229541622bbf1fd4198618d9f677e5/webcolors.json', function (error, response, body) {
   
+
   if (!error && response.statusCode == 200) {
     
     var validColors = JSON.parse(body);
    // console.log(validColors);
-    var colorName = process.argv[2].toLowerCase;
+    var colorName = process.argv[2].toLowerCase();
 
 
     var getRgbValues = function(colorName){
 
-    	var colorKey = _.find(validColors, function(obj){
-    		obj.name.toLowerCase() === colorName.toLowerCase()
-    	console.log(colorKey);
-    	return _.values(colorKey)
-    }
+    	var colorKey = _.find(validColors, function(color){
+    	   
+         return color.name.toLowerCase() === colorName
+      });
+
+    	console.log(colorKey.name + ': ' +colorKey.rgb.r + ', ' + colorKey.rgb.g  + ', ' + colorKey.rgb.b);
     		
     };
 
-    console.log(getRgbValues(colorName));
-  }
+   getRgbValues(colorName);
+  };
 
 });
